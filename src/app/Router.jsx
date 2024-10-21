8c48feddbd6153f124c337d5bc38940ca50a85ba
unknown <hectorruiz572@gmail.com>
2024-10-21 15:37:14 +0200

    BrowserRouter

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -1,11 +1,11 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Alta from "../pages/Alta";
 import Baja from "../pages/Baja";
 import Consulta from "../pages/Consulta";
 import Layout from "../Layout/Layout";
 
 const Router = () => (
-  <HashRouter basename="/react">
+  <BrowserRouter basename="/react">
     <Layout>
       <Routes>
         <Route index element={<Consulta />} />
@@ -13,7 +13,7 @@ const Router = () => (
         <Route path="/Baja" element={<Baja />} />
       </Routes>
     </Layout>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export default Router;
